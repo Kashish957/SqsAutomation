9f1de8d2294b2d5cfbe83a9a00745b5f4cbeaa42
kashish.ahuja <kashish.ahuja@globallogic.com>
2022-08-20 00:15:46 +0530

    warnings removed

--- src/main/java/handler/LambdaHandler.java
+++ src/main/java/handler/LambdaHandler.java
@@ -1,43 +1,35 @@
 package handler;
+
 import com.amazonaws.auth.*;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 import com.amazonaws.services.cognitoidp.model.ListUsersRequest;
-////import  software.amazon.awssdk.services.cognitoidentityprovider.model.ListUsersRequest;
 import com.amazonaws.services.cognitoidp.model.ListUsersResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.AmazonSQSClient;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.*;
-import java.io.FileInputStream;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
-
 import org.json.JSONObject;
-public class LambdaHandler implements RequestHandler<Object, Object>
-{
+
+public class LambdaHandler implements RequestHandler<Object, Object> {
     private static final String simpleQueue = "dead-letter_queue.fifo";
     private AWSCognitoIdentityProvider client;
 
     final AmazonSQS sqs = AmazonSQSClientBuilder.standard().build();
 
-
-
-    public Object handleRequest(Object i, Context o)
-    {
+    public Object handleRequest(Object i, Context o) {
         client = createCognitoClient();
         String queueUrl = getQueueUrl(simpleQueue);
         System.out.println(queueUrl);
         List<Message> messages = getMessagesFromQueue(queueUrl);
         System.out.println("Below are the Messages present in DLQ:---");
-        for (Message msg : messages)
-        {
-            try
-            {
+        for (Message msg : messages) {
+            try {
                 System.out.println(msg.getBody());
                 JSONObject msgBody = new JSONObject(msg.getBody());
                 JSONObject msgAttr = new JSONObject(msgBody.get("MessageAttributes"));
@@ -46,15 +38,14 @@ public class LambdaHandler implements RequestHandler<Object, Object>
                 String subId = (new JSONObject(msgAttr.get("sub").toString())).get("Value").toString();
                 System.out.println(subId + "\n" + email + "\n" + timestamp);
                 checkEmailInCognito(email);
-            }
-            catch (Exception e)
-            {
+            } catch (Exception e) {
                 // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
         return null;
     }
+
     public String getQueueUrl(String queueName) {
         GetQueueUrlRequest getQueueUrlRequest = new GetQueueUrlRequest(queueName);
         return sqs.getQueueUrl(getQueueUrlRequest).getQueueUrl();
@@ -62,31 +53,23 @@ public class LambdaHandler implements RequestHandler<Object, Object>
 
     public List<Message> getMessagesFromQueue(String queueUrl) {
         ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl);
-        List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
-        return messages;
+        return sqs.receiveMessage(receiveMessageRequest).getMessages();
     }
-//    public interface AWSCognitoIdentityProvider{
-//        String checkEmailInCognito(String email);
-//    }
 
-
-    private AWSCognitoIdentityProvider createCognitoClient(){
+    private AWSCognitoIdentityProvider createCognitoClient() {
         AWSCredentials cred = new BasicAWSCredentials("AKIAXJ752B2RV23BZUHH", "9iFkz5jjTq90CaIW/eSZ9B4gW6G5Zj6eDXpUe+sv");
         AWSCredentialsProvider credProvider = new AWSStaticCredentialsProvider(cred);
         return AWSCognitoIdentityProviderClientBuilder.standard().withCredentials(credProvider).withRegion(Regions.US_EAST_1).build();
     }
-    public String checkEmailInCognito(String email) {
+
+    public void checkEmailInCognito(String email) {
         String filter = "email = \"" + email + "\"";
         ListUsersRequest l = new ListUsersRequest();
-        l.setAttributesToGet(new ArrayList<String>());
+        l.setAttributesToGet(new ArrayList<>());
         l.setFilter(filter);
         l.setUserPoolId(" us-east-1_DJpISoGFK");
         ListUsersResult result = client.listUsers(l);
-       result.getUsers().forEach(user->{
-           System.out.println("User with filter applied " + user.getUsername() + " Status " + user.getUserStatus()
-                   + " Created " + user.getUserCreateDate());
-       });
-
-        return null;
+        result.getUsers().forEach(user -> System.out.println("User with filter applied " + user.getUsername() + " Status " + user.getUserStatus()
+                + " Created " + user.getUserCreateDate()));
     }
 }
