156107309cd2c08775ac2ad5b1042f5ee7f00848
kashish.ahuja <kashish.ahuja@globallogic.com>
2022-08-30 22:13:08 +0530

    made some changes

--- src/main/java/handler/LambdaHandler.java
+++ src/main/java/handler/LambdaHandler.java
@@ -5,17 +5,15 @@ import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProvider;
 import com.amazonaws.services.cognitoidp.AWSCognitoIdentityProviderClientBuilder;
 import com.amazonaws.services.cognitoidp.model.ListUsersRequest;
-import com.amazonaws.services.cognitoidp.model.ListUsersResult;
+import com.amazonaws.services.cognitoidp.model.UserType;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
 import com.amazonaws.services.sqs.model.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.json.JSONObject;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class LambdaHandler implements RequestHandler<Object, Object> {
     private static final String simpleQueue = "dead-letter_queue.fifo";
@@ -26,7 +24,6 @@ public class LambdaHandler implements RequestHandler<Object, Object> {
     public Object handleRequest(Object o, Context c) {
         client = createCognitoClient();
         String queueUrl = sqs.getQueueUrl(simpleQueue).getQueueUrl();
-        System.out.println(queueUrl);
         List<Message> messages = getMessagesFromQueue(queueUrl);
         System.out.println("Below are the Messages present in DLQ:---");
        // while(messages.size()>0) {
@@ -34,20 +31,24 @@ public class LambdaHandler implements RequestHandler<Object, Object> {
                 try {
                     String[] l = msg.getBody().split("\"");
                     String email="";
-                    String sub="";
+                    String subId="";
                     String Timestamp="";
                     for (int i = 0; i < l.length; i++) {
                         Timestamp = (l[i].contains("Timestamp"))?l[i + 2]:Timestamp;
-                        sub = (l[i].contains("sub"))?l[i + 8]:sub;
+                        subId = (l[i].contains("sub"))?l[i + 8]:subId;
                         email=(l[i].contains("email"))?l[i + 8]:email;
                     }
                     System.out.println("Timestamp is " + Timestamp);
-                    System.out.println("sub is " + sub);
+                    System.out.println("sub is " + subId);
                     System.out.println("email is " + email);
+                    String primaryUserSubId=getPrimaryUserSubId(email);
+                    if(subId!=primaryUserSubId)
+                    {
+                        String msgBody = msg.getBody();
 
-                    checkEmailInCognito(email);
-                    //if email exist and user is not primary replace subid with primary users subid
-
+                        //"sub"(.)*} -> "sub":type.....
+                        msgBody.replace("\\^sub.*}",primaryUserSubId);
+                    }// how to find index of } after "sub" ..
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -55,20 +56,28 @@ public class LambdaHandler implements RequestHandler<Object, Object> {
        // }
         return null;
     }
-    public void checkEmailInCognito(String email) {
+    public String getPrimaryUserSubId(String email) {
         String filter = "email = \"" + email + "\"";
         ListUsersRequest l = new ListUsersRequest();
         l.setAttributesToGet(new ArrayList<>());
         l.setFilter(filter);
         l.setUserPoolId("us-east-1_DJpISoGFK");
-        ListUsersResult result = client.listUsers(l);
-        result.getUsers().forEach(user ->
+       List<UserType> result = client.listUsers(l).getUsers();
+        AtomicReference<Date> oldestDate= new AtomicReference<>(new Date());
+        AtomicReference<String> oldestUserSubId= new AtomicReference<>("");
+        result.forEach(user ->
         {
             System.out.println("User with filter applied " + user.getUsername() + " Status " + user.getUserStatus()
                     + " Created " + user.getUserCreateDate());
-
-
+            Date createdDate = user.getUserCreateDate();
+            if(oldestDate.get().compareTo(createdDate)<0)
+            {
+                oldestDate.set(createdDate);
+                oldestUserSubId.set(user.getUsername());
+            }
         });
+        return oldestUserSubId.toString();
+
     }
 
     public List<Message> getMessagesFromQueue(String queueUrl) {
